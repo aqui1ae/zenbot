579d288396c8a3ab31d49337257ac8d61a0a2539
Johnathan James <haxwell@gmail.com>
2018-02-03 16:54:58 -0700

    Implementing StrategyService

---
+++ test/_specs/lib/services/strategy-service.test.js
@@ -0,0 +1,8 @@
+var service = require('../../../../lib/services/strategy-service')
+var exchangeServiceFactory = require('../../../../test/_mocks/exchangeService.mock.factory')()
+
+describe('Strategy Service', function() {
+	xit('is skipped', function() {
+
+	})
+})
