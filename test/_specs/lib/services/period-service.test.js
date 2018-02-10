a493ceeaba9c10174bcff77087dd89bbfd6598f0
Johnathan James <haxwell@gmail.com>
2018-02-09 19:20:42 -0700

    Implemented a period-service test

--- test/_specs/lib/services/period-service.test.js
+++ test/_specs/lib/services/period-service.test.js
@@ -9,20 +9,37 @@ describe('Period Service', function() {
 
   beforeEach(function() {
     mock('../../../../lib/services/collection-service', exchangeServiceFactory)
-    service = mock.reRequire('../../../../lib/services/products-service')
+    service = mock.reRequire('../../../../lib/services/period-service')
   })
 
   it('is available', function() {
     expect(service).not.toBe(undefined)
   })
 
-  it('returns true if a timeInMillis falls within a given Period', function() {
+  it('returns a valid Period object from the init() method', function() {
     var instance = service(conf)
 
-    var time
-    var periodObject
+    var rtn = instance.init({trade_id:3000, price:7.55, time:1517787104900}, '3m')
 
-    /*var rtn =*/ instance.isTimeWithinPeriod(time, periodObject)
+    expect(rtn).toBeDefined()
+    expect(rtn.period_id).toBe('3m8432150')
+    expect(rtn.size).toBe('3m')
+    expect(rtn.time).toBe(1517787000000)
+    expect(rtn.open).toBe(7.55)
+    expect(rtn.close).toBe(7.55)
+    expect(rtn.high).toBe(7.55)
+    expect(rtn.low).toBe(7.55)
+    expect(rtn.close_time).toBe(null)
+    expect(rtn.volume).toBe(0)
+  })
+
+  it('returns true if a timeInMillis falls within a given Period', function() {
+    // var instance = service(conf)
+
+    // var time = 1517787104900
+    // var periodObject 
+
+    // /*var rtn =*/ instance.isTimeWithinPeriod(time, periodObject)
 
     //expect(rtn).toBe(true)
   })
