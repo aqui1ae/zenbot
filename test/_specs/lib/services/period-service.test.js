6a591143049c5e44247e79514a33b0b5f0c01bec
Johnathan James <haxwell@gmail.com>
2018-02-09 19:29:28 -0700

    Implemented a period-service test

--- test/_specs/lib/services/period-service.test.js
+++ test/_specs/lib/services/period-service.test.js
@@ -33,6 +33,26 @@ describe('Period Service', function() {
     expect(rtn.volume).toBe(0)
   })
 
+  it('returns a valid Period object from the update() method', function() {
+    var instance = service(conf)
+
+    var trade1Timestamp = 1517787104900
+    var rtn = instance.init({trade_id:3000, size: 1, price:7.55, time:trade1Timestamp}, '3m')
+    
+    rtn = instance.update({trade_id:3023, size: 2, price:10.00, time:trade1Timestamp + 6500}) // add 65 seconds
+
+    expect(rtn).toBeDefined()
+    expect(rtn.period_id).toBe('3m8432150')
+    expect(rtn.size).toBe('3m')
+    expect(rtn.time).toBe(1517787000000)
+    expect(rtn.open).toBe(7.55)
+    expect(rtn.close).toBe(10.00)
+    expect(rtn.high).toBe(10.00)
+    expect(rtn.low).toBe(7.55)
+    expect(rtn.close_time).toBe(trade1Timestamp + 6500)
+    expect(rtn.volume).toBe(2)
+  })
+
   it('returns true if a timeInMillis falls within a given Period', function() {
     // var instance = service(conf)
 
