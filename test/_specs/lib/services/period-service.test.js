03dec69607eedd86159c9b84b4ace5b31196492c
Johnathan James <haxwell@gmail.com>
2018-02-09 22:13:20 -0700

    Implementing and tightening tests in period-service.test..

--- test/_specs/lib/services/period-service.test.js
+++ test/_specs/lib/services/period-service.test.js
@@ -39,7 +39,7 @@ describe('Period Service', function() {
     var trade1Timestamp = 1517787104900
     var rtn = instance.init({trade_id:3000, size: 1, price:7.55, time:trade1Timestamp}, '3m')
     
-    rtn = instance.update({trade_id:3023, size: 2, price:10.00, time:trade1Timestamp + 6500}) // add 65 seconds
+    rtn = instance.update({trade_id:3023, size: 2, price:10.00, time:trade1Timestamp + 65000}) // add 65 seconds
 
     expect(rtn).toBeDefined()
     expect(rtn.period_id).toBe('3m8432150')
@@ -49,18 +49,19 @@ describe('Period Service', function() {
     expect(rtn.close).toBe(10.00)
     expect(rtn.high).toBe(10.00)
     expect(rtn.low).toBe(7.55)
-    expect(rtn.close_time).toBe(trade1Timestamp + 6500)
+    expect(rtn.close_time).toBe(trade1Timestamp + 65000)
     expect(rtn.volume).toBe(2)
   })
 
   it('returns true if a timeInMillis falls within a given Period', function() {
-    // var instance = service(conf)
+    var instance = service(conf)
 
-    // var time = 1517787104900
-    // var periodObject 
+    var trade1Timestamp = 1517787104900
+    var periodObject = instance.init({trade_id:3000, size: 1, price:7.55, time:trade1Timestamp}, '3m')
 
-    // /*var rtn =*/ instance.isTimeWithinPeriod(time, periodObject)
-
-    //expect(rtn).toBe(true)
+    expect(instance.isTimeWithinPeriod(trade1Timestamp, periodObject)).toBe(true)
+    expect(instance.isTimeWithinPeriod(trade1Timestamp + 65000, periodObject)).toBe(true)
+    expect(instance.isTimeWithinPeriod(trade1Timestamp + 180000, periodObject)).toBe(false)
+    // expect(instance.isTimeWithinPeriod(trade1Timestamp - 1000, periodObject)).toBe(false) // TODO: Understand: How does timebucket determine the beginning and end of a bucket?
   })
 })
